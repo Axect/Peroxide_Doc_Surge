c10bba231340fe156261e317be9448998f51e8b5
Axect <axect@outlook.kr>
2021-01-16 23:50:06 +0900

    CSS/JS Update

--- surge.sh
+++ surge.sh
@@ -2,6 +2,13 @@ rm -rf docs/
 cp -r $HOME/Documents/Project/Rust_Project/Peroxide/target/doc/peroxide docs/
 cp -r $HOME/Documents/Project/Rust_Project/Peroxide/target/doc/peroxide docs/peroxide
 cp -r $HOME/Documents/Project/Rust_Project/Peroxide/target/doc/src docs/src
+yes | cp ../Peroxide/target/doc/*.js ./
+yes | cp ../Peroxide/target/doc/*.css ./
+yes | cp ../Peroxide/target/doc/*.svg ./
+yes | cp ../Peroxide/target/doc/*.png ./
+yes | cp ../Peroxide/target/doc/*.txt ./
+yes | cp ../Peroxide/target/doc/*.woff ./
+yes | cp ../Peroxide/target/doc/*.html ./
 cp *.css docs/
 cp *.js docs/
 cp *.woff docs/
